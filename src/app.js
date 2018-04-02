51a2e40a996a837dc822e458525f24b2cf419306
Stephanie B Burr <sbeusse@gmail.com>
2018-04-02 17:03:15 -0400

    timer issues resolved

--- src/app.js
+++ src/app.js
@@ -1,33 +1,10 @@
 // eslint-env jquery
-$(document).ready(function() {
+function gamePageFunction() {
   let surfer = $('.surfer');
   let turtle = $('.turtle');  // maybe should delete this????
   let turtlePositionArray = [];
   let timePenaltyArray = [];
 
-//**********COLLISION DETECTION*******************
-  // window.setInterval(detectCollision, 330);
-
-  function detectCollision() {
-    let surferWidth, surferHeight, surferX, surferY,
-        turtleWidth, turtleHeight, turtleX, turtleY;
-    let turtle = $('.background-image div:last-child');
-
-    surferWidth = surfer.outerWidth();  //Width, Height, X & Y check of surfer's position
-    surferHeight = surfer.outerHeight();
-    surferX = surfer.offset().left;
-    surferY = surfer.offset().top;
-    turtleWidth = turtle.outerWidth(); //Width, Height, X & Y check of turtle's position
-    turtleHeight = turtle.outerHeight();
-    turtleX = turtle.offset().left;
-    turtleY = turtle.offset().top;
-
-    if ((surferX + surferWidth) > (turtleX) && surferX < (turtleX + turtleWidth) &&
-        (surferY + surferHeight) > (turtleY) && surferY <(turtleY + turtleHeight)) {
-      turtle.css('display', 'none');
-      timePenaltyArray.push('hit'); //push to an array and access sum of array for Total Count
-      }
-  }
 
 //****************KEYDOWN FUNCTION************************
   $('body').keydown( function(event) {
@@ -68,8 +45,30 @@ $(document).ready(function() {
   }
 
 function startGame() {
-
+  console.log("i think this works");
+  //**********COLLISION DETECTION*******************
   window.setInterval(detectCollision, 330);
+
+  function detectCollision() {
+    let surferWidth, surferHeight, surferX, surferY,
+        turtleWidth, turtleHeight, turtleX, turtleY;
+    let turtle = $('.background-image div:last-child');
+
+    surferWidth = surfer.outerWidth();  //Width, Height, X & Y check of surfer's position
+    surferHeight = surfer.outerHeight();
+    surferX = surfer.offset().left;
+    surferY = surfer.offset().top;
+    turtleWidth = turtle.outerWidth(); //Width, Height, X & Y check of turtle's position
+    turtleHeight = turtle.outerHeight();
+    turtleX = turtle.offset().left;
+    turtleY = turtle.offset().top;
+
+    if ((surferX + surferWidth) > (turtleX) && surferX < (turtleX + turtleWidth) &&
+        (surferY + surferHeight) > (turtleY) && surferY <(turtleY + turtleHeight)) {
+      turtle.css('display', 'none');
+      timePenaltyArray.push('hit'); //push to an array and access sum of array for Total Count
+      }
+    }
 
     //***********MAKE AND LAUNCH TURTLES****************
   const turtleGenerator = setInterval(makeAndMoveTurtles, 3000); // if less that 3 sec intervals then
@@ -78,7 +77,8 @@ function startGame() {
       let y = Math.floor(Math.random() * (window.innerHeight)); //adds random start location of turtles
                                                                 //within height of window
       turtlePositionArray.push(y);
-  }
+    }
+
     function makeAndMoveTurtles() {
     randomTurtles();
     for (let i = 0; i < turtlePositionArray.length; i++) {
@@ -99,23 +99,27 @@ function startGame() {
       let seconds = 0;
 
       function startTimer() {
+        console.log(seconds);
         if (seconds < 20) {
         $('.seconds').html(seconds += 1);
       } else {
         stopTimer();
-        endGame();
-        seconds = $('.seconds').html();
         scale();
+        endGame();
       }
 
       function stopTimer() {
         clearInterval(timer);
       }
-    }
+
+//********STOPS BG MOVEMENT AND STOPS MAKING NEW TURTLES************
+    function endGame() {
+    $('.background-image').css('animation-play-state', 'paused');
+    stopTurtles();
+  }
 //*************SCALE TO SEE HOW WELL YOU DID*****************
     function scale() {
-      let totalTime = parseInt(seconds) + (timePenaltyArray.length*5);
-      console.log(totalTime);
+      let totalTime = parseInt($('.seconds').html()) + (timePenaltyArray.length*5);
       if (totalTime === 20) {
         alert("Cowabunga Dude! You dodged all the turtles!\nHit reset to play again!");
       } else if (totalTime > 20 && totalTime < 30) {
@@ -124,12 +128,7 @@ function startGame() {
         alert("Sorry Bro! You may need to go back to surf school!\nHit reset to play again!")
       }
     }
-//********STOPS BG MOVEMENT AND STOPS MAKING NEW TURTLES************
-    function endGame() {
-    $('.background-image').css('animation-play-state', 'paused');
-    stopTurtles();
   }
 }
 startGame();
-
-});
+};
